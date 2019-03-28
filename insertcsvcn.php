a4efbd98f2adee18a90c4d3a795ae0ac94b7fde0
JordiM <jordimarzo69@gmail.com>
2019-03-28 09:16:07 +0100

    correction bug insert num CN

--- insertcsvcn.php
+++ insertcsvcn.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Copyright Laurent ROBIN CNRS - Université d'Orléans 2011 
+Copyright Laurent ROBIN CNRS - Université d'Orléans 2011
 Distributeur : UGCN - http://chimiotheque-nationale.org
 
 Laurent.robin@univ-orleans.fr
@@ -9,7 +9,7 @@ Université d’Orléans
 Rue de Chartre – BP6759
 45067 Orléans Cedex 2
 
-Ce logiciel est un programme informatique servant à la gestion d'une chimiothèque de produits de synthèses. 
+Ce logiciel est un programme informatique servant à la gestion d'une chimiothèque de produits de synthèses.
 
 Ce logiciel est régi par la licence CeCILL soumise au droit français et respectant les principes de diffusion des logiciels libres.
 Vous pouvez utiliser, modifier et/ou redistribuer ce programme sous les conditions de la licence CeCILL telle que diffusée par le CEA,
@@ -21,9 +21,9 @@ En contrepartie de l'accessibilité au code source et des droits de copie, de mo
 
 A cet égard l'attention de l'utilisateur est attirée sur les risques associés au chargement, à l'utilisation, à la modification et/ou au développement
  et à la reproduction du logiciel par l'utilisateur étant donné sa spécificité de logiciel libre, qui peut le rendre complexe à manipuler et qui le
-réserve donc à des développeurs et des professionnels avertis possédant des connaissances informatiques approfondies. Les utilisateurs sont donc 
+réserve donc à des développeurs et des professionnels avertis possédant des connaissances informatiques approfondies. Les utilisateurs sont donc
 invités à charger et tester l'adéquation du logiciel à leurs besoins dans des conditions permettant d'assurer la sécurité de leurs systèmes et ou de
- leurs données et, plus généralement, à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+ leurs données et, plus généralement, à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
 
 Le fait que vous puissiez accéder à cet en-tête signifie que vous avez pris connaissance de la licence CeCILL, et que vous en avez accepté les
 termes.
@@ -42,7 +42,7 @@ if ($row[0]=='{ADMINISTRATEUR}') {
 	//suppression des fichier .tmp
 	$mask = REPTEMP."*.tmp";
 	array_map( "unlink", glob( $mask ) );
-	
+
 	if(($fic = fopen($_FILES['fileCN']['tmp_name'], 'r'))!==FALSE) {
 		$sql="UPDATE produit SET pro_num_cn='' WHERE pro_num_cn<>''";
 		$updat=$dbh->exec($sql);
@@ -53,19 +53,24 @@ if ($row[0]=='{ADMINISTRATEUR}') {
 		$contenu_fichier = file_get_contents($_FILES['fileCN']['tmp_name']);
 		$nombre_ligne_fichier = substr_count( $contenu_fichier, "\n" );
 		while(($donne = fgetcsv($fic,30,";"))!=FALSE) {
-			
+
 			// 2 est le nombre de champs du fichier numéro local + numéro national
-			for ($k=0; $k<2; $k++) { 
+			for ($k=0; $k<2; $k++) {
 				if(preg_match("/CN/",$donne[$k])) {
 					if(preg_match("/V/",$donne[$k])) $numcnp=$donne[$k];
 					else $numcnpl=$donne[$k];
 				}
-				else $numlocal=$donne[$k];	
+				else $numlocal=$donne[$k];
 			}
 			if(!empty($numcnp) and !empty($numlocal)) {
-				$sql.="UPDATE produit SET pro_num_cn='$numcnp' WHERE  pro_numero='$numlocal' or pro_num_constant='$numlocal';\n";
+				$numlocal = str_replace("\xEF\xBB\xBF", '', trim($numlocal));
+				$sql.="UPDATE produit SET pro_num_cn='$numcnp' WHERE  pro_numero='$numlocal'";
+				if (is_numeric($numlocal)){
+					$sql .= "or pro_num_constant='$numlocal'";
+				}
+				$sql .= ";\n";
 			}
-			
+
 			//if(isset($numaff) and $numaff==0) $erreur.=ERRORCSV.$numcnp.ERRORCSV1.$numlocal.ERRORCSV2."<br/>";
 			unset ($numcnpl,$numcnp,$numlocal,$numaff);
 			$o++;
@@ -99,7 +104,7 @@ if ($row[0]=='{ADMINISTRATEUR}') {
 		fclose($fic);
 	}
 	if(!empty($erreur)) echo "<br/><br/><br/><br/><br/><br/><br/><br/><p align=\"center\" class=\"erreur\">".$erreur."</p>";
-	else echo "<br/><br/><br/><br/><br/><br/><br/><br/><p align=\"center\" class=\"sauvegarde\">".TRANSREUSSI."</p>";	
+	else echo "<br/><br/><br/><br/><br/><br/><br/><br/><p align=\"center\" class=\"sauvegarde\">".TRANSREUSSI."</p>";
 }
 else require 'deconnexion.php';
 unset($dbh);
