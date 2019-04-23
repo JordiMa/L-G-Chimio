c75982035c28025435927afbb0984bb54726fb62
JordiM <jordimarzo69@gmail.com>
2019-04-23 15:53:24 +0200

    MAJ Wiki

--- wiki/conf/users.auth.php
+++ wiki/conf/users.auth.php
@@ -7,3 +7,7 @@
 # Format:
 #
 # login:passwordhash:Real Name:email:groups,comma,separated
+
+
+kiet:$2y$10$/vq2YxYgZ/e5nJP9dffUTeR0BqHjhmQg2cYD4V8AS2hb.9eGtHCi.:Kiet Tran:kiet.tran@enscm.fr:admin,user
+jordi:$2y$10$8ykP2TuLCoYz6WerXdScj.EgyIYr50CY5RVAwTEUPQJZd6THya/Qu:Jordi Marzo:jordimarzo.pro@gmail.com:admin,user
